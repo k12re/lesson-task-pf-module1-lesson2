f6ea9a37a05fd604c0ab230b758cea1d3380cc8f
k12re <83732570+k12re@users.noreply.github.com>
2022-05-19 23:33:20 +0200

    Did assignment

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//Question 1
+var number = 10;
+var decimal = 10.1;
+
+//Question 2
+var workingDays = "Monday" - "Tuesday" - "Wednesday" - "Thursday" - "Friday";
+
+//Question 3
+var addition = number + decimal;
+var subtraction = decimal - number;
+var multiplication = number * number;
+var division = number / number;
+
+//Question 4
+var lie = false;
+
+//Question 5
+var statement = "The frogs are angry";
+console.log(statement.length);
+
+//Question 6
+var timeElapsed;
+var totalPrice;
+
+//Question 7
+var number1 = "53";
+var number2 = "44.6";
+
+var properNumber1 = parseInt(number1);
+var properNumber2 = parseFloat(number2);
+console.log(number1, number2);
+
+//Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+console.log("animal:", typeof pet);
+console.log("age:", typeof age);
+console.log("sale:", typeof discountApplied, "duration:", typeof timeLeft);
+
+//Question 9
+var headingText = "Updating heading";
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+//Question 10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
